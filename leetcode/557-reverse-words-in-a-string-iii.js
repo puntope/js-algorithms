f5e1395a48e312db75d56d74f3f1561a88fb3665
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2021-10-16 11:53:42 +0200

    Create 557-reverse-words-in-a-string-iii.js

---
+++ leetcode/557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,48 @@
+/**
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+**/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    
+    let r = 0, sOutput = '', wOutput = '';
+    
+    while (r < s.length) {
+        
+        if (s[r] === ' ') {    
+            sOutput += wOutput + ' ';
+            wOutput = '';
+        } else {
+            wOutput = s[r] + wOutput;
+        }
+        
+        r++;
+    }
+    
+    sOutput += wOutput;
+    
+    return sOutput;
+};
