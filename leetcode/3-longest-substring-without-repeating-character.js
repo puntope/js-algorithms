b80a3ad4f67023aa77304b93de4bf39b26c5381c
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2021-10-20 12:50:59 +0200

    Update 3-longest-substring-without-repeating-character.js

--- leetcode/3-longest-substring-without-repeating-character.js
+++ leetcode/3-longest-substring-without-repeating-character.js
@@ -3,23 +3,31 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let hash = {}
-    let lastSubstr = '';
-    let biggest = lastSubstr;
-    for (let j = 0; j < s.length; j++) {
-        if ( hash[s[j]] != undefined) {
-            j = hash[s[j]];
-            hash = {};
-            lastSubstr = '';
-            continue;
+  
+    let pointer = 0;
+    let hash = {};
+    let longest = 0;
+    let current = 0;
+    
+    while (pointer < s.length) {
+        let letter = s[pointer];
+        if (hash[letter] !== undefined) {    
+           
+            if (current > longest) {
+                longest = current;
+            }
+            
+            current = 0;
+            pointer = hash[letter];
+            hash = {}
+        } else {
+            hash[letter] = pointer;
+            current++;
         }
         
-        hash[s[j]] = j;
-        lastSubstr += s[j];
-        if (lastSubstr.length > biggest.length) {
-            biggest = lastSubstr;
-        }
+        
+        pointer++;
     }
     
-    return biggest.length;
+    return current > longest ? current : longest;
 };
